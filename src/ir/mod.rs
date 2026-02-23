d9110bd55146a5db7e68a5316485e9efc9163ddc
Eira <skuldnorniern@gmail.com>
2026-02-23 10:52:22 +0900

    feat: expose new ir code

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,5 +1,11 @@
 pub mod bytecode;
+pub mod compile;
+pub mod disasm;
 pub mod hir;
+pub mod lower;
 
-pub use bytecode::BytecodeChunk;
+pub use bytecode::{BytecodeChunk, ConstEntry, Opcode};
+pub use disasm::disassemble;
+pub use compile::{hir_to_bytecode, CompiledFunction};
 pub use hir::HirFunction;
+pub use lower::{script_to_hir, LowerError};
