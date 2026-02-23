029458a035146b066874004541be30a0196b1af3
Eira <skuldnorniern@gmail.com>
2026-02-23 10:29:29 +0900

    feat: add run to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if let Err(e) = jsina::cli::run(&args) {
+        eprintln!("error: {}", e);
+        std::process::exit(1);
+    }
 }
